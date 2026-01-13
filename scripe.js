5f1996bc858b1f083c9884d8f3d630378bd1b21e
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2026-01-13 20:04:13 +0400

    Update scripe.js

--- scripe.js
+++ scripe.js
@@ -1,44 +1,103 @@
 
 
-// document.getElementById('h');
+        // document.getElementById('h');
 
 
-let name ='ali';
-// name='ali';
+        let name ='ali';
+        // name='ali';
 
-let firstname='ali',lastname ='ahmed';
-firstname=1;
-lastname=2;
-// const variable (we can't re assaig const )
+        let firstname='ali',lastname ='ahmed';
+        firstname=1;
+        lastname=2;
+        // const variable (we can't re assaig const )
 
-const firstname1=1; 
-
-
-
-//
-let true_false=true;
-let nullo=null;
-
-a=typeof nullo;
-console.log("-----");
-console.log(a);
+        const firstname1=1; 
 
 
 
+        //
+        let true_false=true;
+        let nullo=null;
+
+        a=typeof nullo;
+        console.log("-----");
+        console.log(a);
 
 
 
 
 
 
-console.log(true_false);
-console.log("-----");
 
-console.log(nullo);
-console.log("-----");
 
-console.log(name);
-console.log("-----");
 
-console.log(firstname+lastname);
-console.log(firstname1+lastname);
+        console.log(true_false);
+        console.log("-----");
+
+        console.log(nullo);
+        console.log("-----");
+
+        console.log(name);
+        console.log("-----");
+
+        console.log(firstname+lastname);
+        console.log(firstname1+lastname);
+
+
+        let person = {
+            name:'mosh',
+            age : 30
+
+        }
+
+        person.age=2;
+        console.log(person);
+
+        //array
+        let selectColor=['red','blue'];
+        selectColor[2]='green';
+        selectColor.push('black');
+        console.log("-----");
+
+        selectColor.shift(0);
+        // console.log("-----");
+
+        console.log(selectColor.length);
+        console.log("-----");
+
+        console.log(selectColor);
+
+
+
+
+
+        // console.log("-----");("-----");("-----");("-----");
+        console.log("----------------------");
+
+
+        //funcation 
+        function sum( x , y){
+
+            let total;
+            total =x+y;
+            
+            return console.log(total);
+
+        }
+
+        sum(5,7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
