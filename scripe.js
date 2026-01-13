07dfb358e90a81fb3b1a3a635101120012f5683d
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2026-01-13 18:31:30 +0400

    Fix syntax errors and improve variable assignments

--- scripe.js
+++ scripe.js
@@ -1,1 +1,20 @@
+
+
+// document.getElementById('h');
+
 console.log("hello");
+
+let name ='ali';
+// name='ali';
+
+let firstname='ali',lastname ='ahmed';
+firstname=1;
+lastname=2;
+// const variable (we can't re assaig const )
+
+const firstname1=1; 
+
+
+console.log(name);
+console.log(firstname+lastname);
+console.log(firstname1+lastname);
