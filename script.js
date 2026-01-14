12d79b56ca0685d70236e0b1d242f374dd4b1f72
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2026-01-15 00:54:59 +0400

    /

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let js =`amazing `;
 
 console.log("hello");
@@ -16,5 +17,15 @@ let job1= 'IT ';
 let job2 ='programmer';
 let job3='MultiTask';
 
+let number1=5;
+console.log(typeof(number1));
+console.log(typeof(null));
 
+*/
+let age =30;
+const fix_value= 1995;
+//will make error {not able to re assignment }
+// fix_value=14;
 
+document.getElementById('mh1').innerHTML='hello';
+// document.getElementById('p1').
