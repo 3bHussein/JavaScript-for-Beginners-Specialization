aa5a53461695e5479aef05c77a9de0ce29d51063
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2026-01-15 00:18:43 +0400

    /

---
+++ script.js
@@ -0,0 +1,16 @@
+let js =`amazing `;
+
+console.log("hello");
+let firstname ='Matilda';
+console.log(firstname);
+
+let name_arabic ='ahmed';
+let $name ='hamad';
+// no need to start with Upper case only on OOP
+let Pname='almed';
+
+
+
+let PI =3.1415;
+
+
