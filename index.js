f9c9380d64654aa46676d1a2024b1db783fbdf36
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2026-01-14 14:38:57 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-console.log("gello");
+console.log("hello");
 console.log("i like C#");
+window.alert("hell");
+
+
+document.getElementById("mh1").textContent="hello";
+document.getElementById("mh1").append(' from js ');
+document.getElementById("p1").textContent='hello i love  hello';
+
+document.getElementById("p1").append("'<strong>'hello from js'<strong>' ");
+
+//
+let x1=23;
+
+var x3=4;
+console.log(x3);
+// window.append(x3);
+window.alert(typeof(x3));
+let age =25;
+let price =300;
+console.log(`hello mark ${age}`);
