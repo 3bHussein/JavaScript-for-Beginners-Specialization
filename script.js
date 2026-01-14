41b25a612b642b1dacf40224d9a5f6f68a3a3bdb
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2026-01-15 01:22:03 +0400

    Update script.js

--- script.js
+++ script.js
@@ -22,10 +22,37 @@ console.log(typeof(number1));
 console.log(typeof(null));
 
 */
+
 let age =30;
 const fix_value= 1995;
 //will make error {not able to re assignment }
 // fix_value=14;
 
-document.getElementById('mh1').innerHTML='hello';
+// document.getElementById('mh1').innerHTML='hello';
 // document.getElementById('p1').
+//x= ageJonas*2+ ('hello ')+agesarah;
+const now =2037;
+const ageJonas =now- 1991;
+const agesarah =now- 1918;
+
+let x1,x2;
+
+x2=4;
+x1=x2;
+console.log(x1);
+
+console.log('--------------------');
+// x1 =+10;
+x1 += 10;
+console.log('--------------------');
+
+console.log(x1);
+let if_conconsole;
+if_conconsole= console.log(x1>15);
+if (if_conconsole) {
+    console.log("wow")
+}
+else{
+    console.log(`bro it's wrong `)
+}
+document.getElementById('p1').append(` +x1 = ${x1}`);
